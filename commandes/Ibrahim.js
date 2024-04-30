251ec743f22c5d6d10bb5c46619cf5149dc0579a
GEEKMD099 <159744285+GEEKMD099@users.noreply.github.com>
2024-04-30 20:30:26 +0100

    Update Ibrahim.js

--- commandes/Ibrahim.js
+++ commandes/Ibrahim.js
@@ -6,7 +6,7 @@ france({ nomCom: "ibrahim", reaction: "🧒", nomFichier: __filename }, async (d
     let z = 'Yes you called me whats up Ok anyway support me by following my channel and subscribing on my youtube channel 🙏 \n\n ' + "║❒ 𝙒𝘼𝘾𝙃𝘼𝙉𝙉𝙀𝙇 https://whatsapp.com/channel/0029VaWQOHsFSAsv3SD3dJ3a";
     let d = '                                        ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/@Geekmdgptx';
     let varmess = z + d;
-    var mp4 = 'https://telegra.ph/file/dec033e457813cd2ff2e3.mp4';
+    var mp4 = 'https://telegra.ph/file/328ffcf6308d2e15c953b.mp4';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
     //console.log("montest")
 });
